919a94028cf8dd31f49798c339191b999ca68fa5
Bandar K. Bawazir <155910476+bandarbwz@users.noreply.github.com>
2026-08-05 13:50:36 +0000

    fix week_02

---
+++ week_02/Main.java
@@ -0,0 +1,13 @@
+package week_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student student = new Student("Ali", 20, 3.75);
+
+        student.displayInfo();
+        student.study();
+        student.takeExam();
+    }
+}
